a62ea2e98fe66c4d1d9eeeac095ea2e35ea40a1d
tawfik97 <tawfik.achag@gmail.com>
2019-04-04 01:28:44 +0100

    rec done

--- app/src/main/java/co/ach/com/onlinemm/MainActivity.java
+++ app/src/main/java/co/ach/com/onlinemm/MainActivity.java
@@ -1,7 +1,14 @@
 package co.ach.com.onlinemm;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+    }
+
+    public void tt(View view) {
+        Intent go =new Intent(MainActivity.this,Songlist.class);
+        startActivity(go);
     }
 }
